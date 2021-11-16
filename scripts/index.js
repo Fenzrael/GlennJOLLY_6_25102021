a84e45c3bb5e9341737cf3b36784fcb52eb7d19a
Glenn_JOLLY <glenn-jolly@hotmail.fr>
2021-11-16 19:16:48 +0100

    Test instauration card recipe

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,41 @@
 const params = new URLSearchParams(document.location.search.substring(1));
+const recipes = document.getElementById("recipes");
+
+//Import all Datas of recipes.json
+const recipesArray = [];
+console.log(recipesArray);
+let recipesData = [];
+
+//Import data of recipes.json
+
+const recipesInfo = async () => {
+  await fetch("./data/recipes.json")
+    .then((res) => res.json())
+    .then((data) => (recipesData = data));
+
+  console.log(recipesData);
+};
+
+const recipesDisplay = async () => {
+  await recipesInfo();
+
+  recipesData.recipe.forEach((recipe) => {
+    recipesArray.push(recipe);
+  });
+};
+
+function constructMediaHtml() {
+  recipes.innerHTML = "";
+  recipesArray.forEach((recipe) => {
+    recipes.innerHTML += `
+        <figure class="recipes__card card">
+            <img class="card__image" src="./assets/Img/cook.png" alt="image"/>
+            <figcaption class="card__description">
+                ${recipe.name}${recipe.time}${recipe.ingredients}${recipe.description}
+            </figcaption>
+        </figure>
+        `;
+  });
+}
+
+constructMediaHtml();
